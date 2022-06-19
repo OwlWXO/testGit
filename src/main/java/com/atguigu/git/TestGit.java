8e27b0769c1ca57d7b6682447db9103fd0db7f60
wangxu <1318272526@qq.com>
2022-06-19 22:47:13 +0800

    author commit

--- src/main/java/com/atguigu/git/TestGit.java
+++ src/main/java/com/atguigu/git/TestGit.java
@@ -1,7 +1,9 @@
 package com.atguigu.git;
 
-
-
+/**
+ * @author Owl
+ * @date 2022/6/19 22:46
+ */
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello git");
